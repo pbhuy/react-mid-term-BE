4775a939ef714d55a7f3993eba56eab44ac6ec63
Bao Huy <pbhuy0909@gmail.com>
2023-11-10 17:58:20 +0700

    deploy server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 
 const connection = require('./config/database');
 const router = require('./api/routes');
@@ -9,8 +9,6 @@ const { sendErr } = require('./api/helpers/response');
 
 const app = express();
 const port = process.env.PORT || 8080;
-
-connection();
 
 // cors
 const corsOptions = {
@@ -28,7 +26,7 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '/uploads')));
 
 // logger
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 // routes
 app.use('/api', router);
@@ -40,6 +38,8 @@ app.use((err, req, res, next) => {
   sendErr(res, { status, message });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+connection().then(
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  }),
+);
